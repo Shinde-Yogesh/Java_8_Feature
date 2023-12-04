9121c9ec4b58fe9ca9788bdeb860d0d71ec9aa92
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-04 20:21:45 +0530

    first char with LinkedHashMap

--- src/com/practice/Question_Asked_In_Companies.java
+++ src/com/practice/Question_Asked_In_Companies.java
@@ -1,6 +1,7 @@
 package com.practice;
 
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -11,7 +12,8 @@ public class Question_Asked_In_Companies {
 		String str = "ilovejavatechi";
 //		number_of_charOccarrance(str);
 //		greaterThanOneCharOccarance(str);
-		uniqCharinStr(str);
+//		uniqCharinStr(str);
+		firstNonRepeatChar(str);
 	}
 	// group by here work like how many copies are there 
 		public static void number_of_charOccarrance(String str)
@@ -65,4 +67,38 @@ public class Question_Asked_In_Companies {
 					.collect(Collectors.toList());
 			System.out.println(uniq);
 		}
+		
+		public static void firstNonRepeatChar (String str)
+		{ 
+			Map<String, List<String>> all_Occarance  =  Arrays.stream(str.split(""))
+					.collect(Collectors.groupingBy(s -> s));
+			System.out.println(all_Occarance);
+			
+			/*
+			 the ans return the c
+			 but it is false
+			 because the it used hash map so insertion order is not maintain
+			 
+			 String fist_non_Repeat = Arrays.stream(str.split(""))
+					.collect(Collectors
+					.groupingBy(Function.identity(),Collectors.counting()))
+					.entrySet()
+					.stream()
+					.filter(x -> x.getValue() == 1)
+					.findFirst()
+					.get()
+					.getKey();
+					*/
+			String fist_non_Repeat = Arrays.stream(str.split(""))
+					.collect(Collectors
+							.groupingBy(Function.identity(),LinkedHashMap :: new,Collectors.counting()))
+					.entrySet()
+					.stream()
+					.filter(x -> x.getValue() == 1)
+					.findFirst()
+					.get()
+					.getKey();
+			
+			System.out.println(fist_non_Repeat);
+		  }
 }
