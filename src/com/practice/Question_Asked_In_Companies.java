be165cfcd57cde1ad4f9600560e52d42be65b976
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-04 20:14:56 +0530

    more than one time the character

--- src/com/practice/Question_Asked_In_Companies.java
+++ src/com/practice/Question_Asked_In_Companies.java
@@ -1,6 +1,7 @@
 package com.practice;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -8,7 +9,8 @@ import java.util.stream.Collectors;
 public class Question_Asked_In_Companies {
 	public static void main(String[] args) {
 		String str = "ilovejavatechi";
-		number_of_charOccarrance(str);
+//		number_of_charOccarrance(str);
+		greaterThanOneCharOccarance(str);
 	}
 	// group by here work like how many copies are there 
 		public static void number_of_charOccarrance(String str)
@@ -23,5 +25,24 @@ public class Question_Asked_In_Companies {
 					.collect(Collectors
 					.groupingBy(Function.identity(),Collectors.counting()));
 			System.out.println(result);
+		}
+		
+		public static void greaterThanOneCharOccarance (String str)
+		{
+			Map<String, List<String>> all_Occarance  =  Arrays.stream(str.split(""))
+				.collect(Collectors.groupingBy(s -> s));
+				System.out.println(all_Occarance);
+			
+			
+			
+			 List<String> duplicateElement = Arrays.stream(str.split(""))
+					.collect(Collectors
+					.groupingBy(Function.identity(),Collectors.counting()))
+					.entrySet()
+					.stream()
+					.filter(x -> x.getValue() > 1)
+					.map(Map.Entry :: getKey)
+					.collect(Collectors.toList());
+		System.out.println(duplicateElement);
 		}
 }
