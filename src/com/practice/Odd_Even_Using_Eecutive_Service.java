d08b60e4576586e450de78573bc5cb56edc12f71
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-11 19:44:12 +0530

    using the executorService and CompletableFuture java 8 feauture

---
+++ src/com/practice/Odd_Even_Using_Eecutive_Service.java
@@ -0,0 +1,39 @@
+package com.practice;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+public class Odd_Even_Using_Eecutive_Service {
+	public static void main(String[] args) {
+		ExecutorService executorService = Executors.newFixedThreadPool(2);
+		IntStream.rangeClosed(1, 10)
+		.forEach(num ->
+		{
+			CompletableFuture<Integer> oddcompletedFuture = CompletableFuture.completedFuture(num)
+				.thenApplyAsync(x ->
+				{
+					if(x%2 != 0)
+					{
+						System.out.println("Thread Name : "+Thread.currentThread().getName()+" value : "+x);
+					}
+					return num;
+				}, executorService);
+		oddcompletedFuture.join();
+		
+		CompletableFuture<Integer> evencompletedFuture = CompletableFuture.completedFuture(num)
+				.thenApplyAsync(x ->
+				{
+					if(x%2 == 0)
+					{
+						System.out.println("Thread Name : "+Thread.currentThread().getName()+" value : "+x);
+					}
+					return num;
+				}, executorService);
+		evencompletedFuture.join();
+		});
+		
+		executorService.shutdown();
+	}
+}
