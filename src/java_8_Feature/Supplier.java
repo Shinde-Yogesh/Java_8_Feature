42eadeaebcd278eb6019b9ee8fb39a1ac1937cc3
Yogesh Shinde <yogeshshinde2324@gmail.com>
2024-07-08 22:40:53 +0530

    Update Supplier.java

--- src/java_8_Feature/Supplier.java
+++ src/java_8_Feature/Supplier.java
@@ -7,11 +7,8 @@ public class Supplier {
 	public static void main(String[] args) {
 /*		Supplier  obj = new Supplier();
 		System.out.println(obj.fetchCurrentDate());
-		
-		
 		Supplier currentDate = () -> new Date();
 		System.out.println(currentDate.get());
-		
 	}
  publid Date Date fetchCurrentDate()
  {
